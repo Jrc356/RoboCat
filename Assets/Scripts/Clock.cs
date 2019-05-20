e74cbf6c0a86da0303295b7e7d53f4a8c6c3e378
Jon Corbin <jon.corbin8@gmail.com>
2019-05-20 00:33:17 -0400

    Add game files

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    private int timeRemainingS;
+    public int totalTimeS = 60;
+    public Text timeText;
+    public Text gameOverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverText.enabled = false;
+        timeRemainingS = totalTimeS;
+        timeText.text = "Time Remaining: " + timeRemainingS.ToString();
+        StartCoroutine("DecreaseTime");
+    }
+
+    void Update() {
+        timeText.text = "Time Remaining: " + timeRemainingS.ToString();
+    }
+    
+    public IEnumerator DecreaseTime() {
+        while (timeRemainingS > 0) {
+            yield return new WaitForSeconds(1.0f);
+            timeRemainingS--;
+        }
+
+        GameOver();
+    }
+
+    public void GameOver() {
+        gameOverText.enabled = true;
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int pointsToAdd = 10;
+    public float playerSpeed;
+    public float walkSpeed = 2f;
+    public float sprintSpeed = 10f;
+    public float mouseSensitivity = 2f;
+    public float jumpHeight = 8f;
+    public float reach = 10f;
+    public float power = 100f;
+    public Camera cam;
+    public GameObject UI;
+
+
+    private float yRot;
+    private float xRot;
+    private Rigidbody rigidBody;
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = walkSpeed;
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // -------------------- Movement --------------------
+        transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0);
+        //transform.Rotate(-Input.GetAxis("Mouse Y") * mouseSensitivity, 0, 0);
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
+            Vector3 target = cam.transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed * Time.deltaTime;
+            target.y = 0;
+            transform.Translate(target, Space.World);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) {
+            Vector3 target = cam.transform.forward * Input.GetAxisRaw("Vertical") * playerSpeed * Time.deltaTime;
+            target.y = 0;
+            transform.Translate(target, Space.World);
+        }
+
+        // -------------------- Jump --------------------
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            rigidBody.velocity += (transform.up * jumpHeight);
+            isGrounded = false;
+        }
+
+        // -------------------- Sprint --------------------
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            playerSpeed = sprintSpeed;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            playerSpeed = walkSpeed;
+        }
+
+        // -------------------- Push --------------------
+        if (Input.GetMouseButtonDown(0)) {
+            Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit);
+            Debug.Log(hit.transform.gameObject.name);
+            if(hit.distance <= reach && hit.transform.gameObject.GetComponent<Destructible>() != null) {
+                hit.transform.gameObject.transform.GetComponent<Destructible>().Kill();
+                UI.GetComponent<Scoring>().addScore(pointsToAdd);
+            }
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        isGrounded = true;
+    }
+}
